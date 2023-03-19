3a945f6866a9f6b4bf0e11432fbdfc9adea9579a
zerleger <101527159+zerleger@users.noreply.github.com>
2023-03-19 20:55:51 +0100

    async demo

---
+++ WPFUserInterface/DemoMethods.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WPFUserInterface
+{
+    public static class DemoMethods
+    {
+        public static List<string> PrepData()
+        {
+            List<string> output = new List<string>();
+
+            output.Add("https://www.yahoo.com");
+            output.Add("https://www.google.com");
+            output.Add("https://www.cnn.com");
+            output.Add("https://www.amazon.com");
+            output.Add("https://www.facebook.com");
+            output.Add("https://www.twitter.com");
+            output.Add("https://www.codeproject.com");
+            output.Add("https://en.wikipedia.org/wiki/.NET_Framework");
+
+            return output;
+        }
+
+        public static List<WebsiteDataModel> RunDownloadSync()
+        {
+            List<string> websites = PrepData();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                output.Add(results);
+            }
+
+            return output;
+        }
+
+        public static List<WebsiteDataModel> RunDownloadParallelSync()
+        {
+            List<string> websites = PrepData();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+
+            Parallel.ForEach<string>(websites, (site) =>
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                output.Add(results);
+            });
+
+            return output;
+        }
+
+
+
+        public static async Task<List<WebsiteDataModel>> RunDownloadParallelAsyncV2(IProgress<ProgressReportModel> progress)
+        {
+            List<string> websites = PrepData();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+            ProgressReportModel report = new ProgressReportModel();
+
+            await Task.Run(() =>
+            {
+                Parallel.ForEach<string>(websites, (site) =>
+                {
+                    WebsiteDataModel results = DownloadWebsite(site);
+                    output.Add(results);
+
+                    report.SitesDownloaded = output;
+                    report.PercentageComplete = (output.Count * 100) / websites.Count;
+                    progress.Report(report);
+                });
+            });
+
+            return output;
+        }
+
+        public static async Task<List<WebsiteDataModel>> RunDownloadAsync(IProgress<ProgressReportModel> progress, CancellationToken cancellationToken)
+        {
+            List<string> websites = PrepData();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+            ProgressReportModel report = new ProgressReportModel();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = await DownloadWebsiteAsync(site);
+                output.Add(results);
+
+                cancellationToken.ThrowIfCancellationRequested();
+
+                report.SitesDownloaded = output;
+                report.PercentageComplete = (output.Count * 100) / websites.Count;
+                progress.Report(report);
+            }
+
+            return output;
+        }
+
+        public static async Task<List<WebsiteDataModel>> RunDownloadParallelAsync()
+        {
+            List<string> websites = PrepData();
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+
+            foreach (string site in websites)
+            {
+                tasks.Add(DownloadWebsiteAsync(site));
+            }
+
+            var results = await Task.WhenAll(tasks);
+
+            return new List<WebsiteDataModel>(results);
+        }
+
+        private static async Task<WebsiteDataModel> DownloadWebsiteAsync(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = await client.DownloadStringTaskAsync(websiteURL);
+
+            return output;
+        }
+
+        private static WebsiteDataModel DownloadWebsite(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = client.DownloadString(websiteURL);
+
+            return output;
+        }
+    }
+}
---
+++ WPFUserInterface/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFUserInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void executeSync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            var results = DemoMethods.RunDownloadParallelSync();
+            PrintResults(results);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: { elapsedMs }";
+        }
+
+        private async void executeAsync_Click(object sender, RoutedEventArgs e)
+        {
+            Progress<ProgressReportModel> progress = new Progress<ProgressReportModel>();
+            progress.ProgressChanged += ReportProgress;
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            try
+            {
+                var results = await DemoMethods.RunDownloadAsync(progress, cts.Token);
+                PrintResults(results);
+            }
+            catch (OperationCanceledException)
+            {
+                resultsWindow.Text += $"The async download was cancelled. { Environment.NewLine }";
+            }
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: { elapsedMs }";
+        }
+
+        private void ReportProgress(object sender, ProgressReportModel e)
+        {
+            dashboardProgress.Value = e.PercentageComplete;
+            PrintResults(e.SitesDownloaded);
+        }
+
+        private async void executeParallelAsync_Click(object sender, RoutedEventArgs e)
+        {
+            Progress<ProgressReportModel> progress = new Progress<ProgressReportModel>();
+            progress.ProgressChanged += ReportProgress;
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            var results = await DemoMethods.RunDownloadParallelAsyncV2(progress);
+            PrintResults(results);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: { elapsedMs }";
+        }
+
+        private void cancelOperation_Click(object sender, RoutedEventArgs e)
+        {
+            cts.Cancel();
+        }
+
+        private void PrintResults(List<WebsiteDataModel> results)
+        {
+            resultsWindow.Text = "";
+            foreach (var item in results)
+            {
+                resultsWindow.Text += $"{ item.WebsiteUrl } downloaded: { item.WebsiteData.Length } characters long.{ Environment.NewLine }";
+            }
+        }
+    }
+}
